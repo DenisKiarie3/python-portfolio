0a02c0c641bb7e793852a6216e12d3f700d37ad0
DenisKiarie3 <deniskiarie3@gmail.com>
2026-02-20 10:09:39 +0300

    Added examples of variable names and assignment

---
+++ variables-expressions/variables_assignment.py
@@ -0,0 +1,21 @@
+#Variable names and assignment examples
+
+#Single assignment
+name = "Denis"
+age = 27
+height = 1.75
+
+#Multiple assignment
+x, y, z = 10, 20, 30
+
+#Swap example
+a = 2
+b = 9
+a, b = b, a
+
+#Print all
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("x, y, z:", x, y, z)
+print("a , b after swap:", a, b)
