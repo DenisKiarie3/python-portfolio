d35fd8db508b2c4526f905d7b1cef1c15e8be3fe
DenisKiarie3 <deniskiarie3@gmail.com>
2026-02-20 10:47:46 +0300

    Added examples of numerical expressionals with operators and parentheses

---
+++ variables-expressions/numerical_expressions.py
@@ -0,0 +1,15 @@
+# Numerical expressions examples
+x = 15 
+y = 4
+
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)
+print("x // y =", x // y) #floor division
+print("x % y =", x % y)
+print("x ** y =", x ** y)
+
+# Combining variables and parentheses
+result = (x + y) * (x - y)
+print("Result of (x + y) * (x - y) =", result)
