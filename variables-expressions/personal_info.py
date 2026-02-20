9c8a21dc80bec871a3a556413ac0583f9922a287
DenisKiarie3 <deniskiarie3@gmail.com>
2026-02-20 09:47:29 +0300

    Add personal info example demonstrating variables and constants

---
+++ variables-expressions/personal_info.py
@@ -0,0 +1,11 @@
+#Store personal information using variables
+NAME = "Denis" #constant(by convention)
+age = 27
+HEIGHT = 1.75 # in meters
+country = "Kenya"
+
+print("Name:", NAME)
+print("Age:", age)
+print("Height:", HEIGHT)
+print("Country:", country)
+print(type(NAME), type(age), type(HEIGHT), type(country))
