f36acee82a51fa8740e0aa269b7bd6332aa50be7
DenisKiarie3 <deniskiarie3@gmail.com>
2026-02-20 23:49:18 +0300

    Added examples demonstrating type conversion using int, float and str

---
+++ variables-expressions/type_conversion.py
@@ -0,0 +1,14 @@
+# Type conversion examples
+
+# Integer conversion
+age = int(input("Enter your age: "))
+print("In 5 years you will be:", age + 5)
+
+# Float conversion
+price = float(input("Enter price: "))
+tax = price * 0.16
+print("Tax (16%):", round(tax, 2))
+
+# String conversion
+number = 100
+print("Number as string:", str(number))
